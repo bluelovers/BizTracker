2a9b26df00fdc92606720f255e80602f701a09e5
demogodyou <demogodyou@d1847493-4e3b-e4a7-23b0-afffdb14bbb4>
2012-01-08 14:31:52 +0000

    Add SHA-2 encrypt method.
    git-svn-id: http://biztracker.googlecode.com/svn/trunk@49 d1847493-4e3b-e4a7-23b0-afffdb14bbb4

--- src/com/xiaolei/android/common/Utility.java
+++ src/com/xiaolei/android/common/Utility.java
@@ -433,6 +433,10 @@ public final class Utility {
 	}
 
 	public static String toMD5String(String plainText) {
+		if (TextUtils.isEmpty(plainText)) {
+			plainText = "";
+		}
+		
 		// Reverse
 		StringBuilder text = new StringBuilder();
 		for (int i = plainText.length() - 1; i >= 0; i--) {
@@ -453,6 +457,42 @@ public final class Utility {
 		byte d[] = mDigest.digest();
 		StringBuffer hash = new StringBuffer();
 
+		for (int i = 0; i < d.length; i++) {
+			hash.append(Integer.toHexString(0xFF & d[i]));
+		}
+
+		return hash.toString();
+	}
+
+	/**
+	 * Use an one way encrypt algorithm to encrypt the plain text then returns the hex string.
+	 * @param plainText plain text you want to encrypt
+	 * @return the encrypted hex string
+	 */
+	public static String encrypt(String plainText) {
+		if (TextUtils.isEmpty(plainText)) {
+			plainText = "";
+		}
+
+		// Reverse
+		StringBuilder text = new StringBuilder();
+		for (int i = plainText.length() - 1; i >= 0; i--) {
+			text.append(plainText.charAt(i));
+		}
+
+		plainText = text.toString();
+
+		MessageDigest mDigest;
+		try {
+			mDigest = MessageDigest.getInstance("SHA-512");
+		} catch (NoSuchAlgorithmException e) {
+			return plainText;
+		}
+
+		mDigest.update(plainText.getBytes());
+		byte d[] = mDigest.digest();
+
+		StringBuffer hash = new StringBuffer();
 		for (int i = 0; i < d.length; i++) {
 			hash.append(Integer.toHexString(0xFF & d[i]));
 		}
