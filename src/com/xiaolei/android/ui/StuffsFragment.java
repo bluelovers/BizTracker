ebd52473ac08c26a648c30fb33a7a52c7c830202
demogodyou@gmail.com <demogodyou@gmail.com@d1847493-4e3b-e4a7-23b0-afffdb14bbb4>
2012-09-29 07:14:15 +0000

    Fix bugs.
    git-svn-id: http://biztracker.googlecode.com/svn/trunk@166 d1847493-4e3b-e4a7-23b0-afffdb14bbb4

--- src/com/xiaolei/android/ui/StuffsFragment.java
+++ src/com/xiaolei/android/ui/StuffsFragment.java
@@ -4,7 +4,6 @@ import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
